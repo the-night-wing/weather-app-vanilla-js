6e13c90ea1f3a7014240baa4847ab6c92d1b9a50
Den <denisanatoliyovich@gmail.com>
2020-01-19 12:31:27 +0200

    Improved error catching system, it turned out that if we are disconnected from the Internet, other type of error is returned. So, it parses the error message and show correct notification, though now it handles only 404 and "offline" :D Also added a panel with a plenty of checkboxes. When the paragrap is clicked on - the panel smoothly slides out. Click once more -- it slides in. Marking different checkboxes we can customize information shown on the screen. In CSS: added animations and styles for other temperature data. Now values are bold and underscored. Also changed the way our data is written into HTML. Now there're separate spans for values, which are nested into p's with description of the value.

--- src/app.js
+++ src/app.js
@@ -9,14 +9,47 @@ const tempActual_p = document.getElementById("temp-act");
 const tempFeels_p = document.getElementById("temp-feels");
 const tempMin_p = document.getElementById("temp-min");
 const tempMax_p = document.getElementById("temp-max");
+const humidity_p = document.getElementById("humidity");
+const pressure_p = document.getElementById("pressure");
+const precipitation_p = document.getElementById("precipitation");
 const getWeather_but = document.getElementById("getWeather");
 const cityInput_inp = document.getElementById("cityInput");
 const countryInput_inp = document.getElementById("countryInput");
 const weatherLogo_icon = document.getElementById("weather-logo");
 const inputError_div = document.getElementById("input-error");
 
-console.log(weatherLogo_icon.classList.value);
+const tempActual_cb = document.getElementsByName("actual-temp-cb")[0];
+const tempFeels_cb = document.getElementsByName("feels-like-cb")[0];
+const tempMin_cb = document.getElementsByName("min-temp-cb")[0];
+const tempMax_cb = document.getElementsByName("max-temp-cb")[0];
+const humidity_cb = document.getElementsByName("humidity-cb")[0];
+const pressure_cb = document.getElementsByName("pressure-cb")[0];
+const precipitation_cb = document.getElementsByName("precipitation-cb")[0];
 
+const forecastOptions_p = document.querySelector(".forecast-options > p");
+const checkboxes_div = document.getElementById("forecast-checkboxes");
+// console.log(weatherLogo_icon.classList.value);
+
+const checkboxes_ = [
+    tempActual_cb,
+    tempFeels_cb,
+    tempMin_cb,
+    tempMax_cb,
+    humidity_cb,
+    pressure_cb,
+    precipitation_cb
+];
+const paragraphs = [
+    tempActual_p,
+    tempFeels_p,
+    tempMin_p,
+    tempMax_p,
+    humidity_p,
+    pressure_p,
+    precipitation_p
+];
+
+let forecastOptions = false;
 getWeather_but.addEventListener("click", e => setWeather(e));
 
 const onCityChange = e => {
@@ -27,8 +60,69 @@ const onCountryChange = e => {
     state.country = e.target.value;
 };
 
+const hideByCheckBox = (el, cb) => {
+    if (cb.checked === false) {
+        // console.log(true);
+        el.parentElement.classList.value = "hide";
+    } else {
+        el.parentElement.classList.value = "temp";
+        // console.log(false);
+    }
+};
+
+for (let i = 0; i < checkboxes_.length; i++) {
+    checkboxes_[i].addEventListener("click", () =>
+        hideByCheckBox(paragraphs[i], checkboxes_[i])
+    );
+}
+
+// tempActual_cb.addEventListener("click", () =>
+//     hideByCheckBox(tempActual_p, tempActual_cb)
+// );
+
+// tempFeels_cb.addEventListener("click", () =>
+//     hideByCheckBox(tempFeels_p, tempFeels_cb)
+// );
+
+// tempMin_cb.addEventListener("click", () =>
+//     hideByCheckBox(tempMin_p, tempMin_cb)
+// );
+
+// tempMax_cb.addEventListener("click", () =>
+//     hideByCheckBox(tempMax_p, tempMax_cb)
+// );
+
+// humidity_cb.addEventListener("click", () =>
+//     hideByCheckBox(humidity_p, humidity_cb)
+// );
+
+// pressure_cb.addEventListener("click", () =>
+//     hideByCheckBox(pressure_p, pressure_cb)
+// );
+
+// precipitation_cb.addEventListener("click", () =>
+//     hideByCheckBox(precipitation_p, precipitation_cb)
+// );
+
+const onForecastOptionsClick = () => {
+    if (!forecastOptions) {
+        checkboxes_div.classList.value = "checkboxes forecast-options-show";
+        forecastOptions = !forecastOptions;
+        console.log("object");
+    } else {
+        checkboxes_div.classList.value = "checkboxes forecast-options-hide";
+        forecastOptions = !forecastOptions;
+        console.log("object11");
+        setTimeout(() => {
+            checkboxes_div.classList.value = "checkboxes hide";
+        }, 2000);
+    }
+};
+
 cityInput_inp.addEventListener("input", e => onCityChange(e));
 countryInput_inp.addEventListener("input", e => onCountryChange(e));
+
+forecastOptions_p.onclick = onForecastOptionsClick;
 
 const getWeatherData = async (city, country) => {
     const api_call = await fetch(
@@ -41,10 +135,17 @@ const getWeatherData = async (city, country) => {
     if (api_call.status !== 200) {
         console.log(api_call.status);
         console.log(api_call.statusText);
+        const errObject = {
+            code: api_call.status,
+            message: api_call.statusText
+        };
+        console.log(JSON.stringify(errObject));
         // return Promise.reject([api_call.status, api_call.statusText]);
-        throw new Error([api_call.status, api_call.statusText]);
+        throw new Error(JSON.stringify(errObject));
 
         // console.log(e);
+    } else {
+        return api_call.json();
     }
     // console.log(response);
     // console.log((response.main.temp - 273.15).toFixed(2));
@@ -55,16 +156,16 @@ const getWeatherData = async (city, country) => {
 
 const setTemperature = weather => {
     const degreeSign = "°";
-    const tempActual = `Actual temperature: ${(
-        weather.main.temp - 273.15
-    ).toFixed(2)}${degreeSign}`;
-    const tempFeels = `Feels like: ${(weather.main.feels_like - 273.15).toFixed(
+    const tempActual = `${(weather.main.temp - 273.15).toFixed(
         2
     )}${degreeSign}`;
-    const tempMax = `Max temp: ${(weather.main.temp_max - 273.15).toFixed(
+    const tempFeels = `${(weather.main.feels_like - 273.15).toFixed(
         2
     )}${degreeSign}`;
-    const tempMin = `Min temp: ${(weather.main.temp_min - 273.15).toFixed(
+    const tempMax = `${(weather.main.temp_max - 273.15).toFixed(
+        2
+    )}${degreeSign}`;
+    const tempMin = `${(weather.main.temp_min - 273.15).toFixed(
         2
     )}${degreeSign}`;
     tempActual_p.innerHTML = tempActual;
@@ -90,22 +191,29 @@ const hideEmptyCityError = () => {
     cityInput_inp.classList.value = "";
 };
 
-const showInputCityError = message => {
-    let errMessage = message;
-    console.log(message);
-    switch (message) {
-        case "TypeError: Failed to fetch":
-            errMessage = "Check your internet connection";
-            break;
+const showInputCityError = error => {
+    const { message: errorMessage } = error;
+    console.log(errorMessage);
+    let errMessageToShow;
+    if (errorMessage === "Failed to fetch") {
+        console.log(true);
+        errMessageToShow = "Check your internet connection";
+    } else {
+        const errObj = JSON.parse(errorMessage);
+        const { code, message } = errObj;
+        if (code === 404) {
+            errMessageToShow = "Such a city was not found";
+        }
+
+        console.log(code);
+        console.log(message);
     }
-    if ((message = "TypeError: Failed to fetch")) {
-        errMessage = "Check your internet connection";
-    }
-    console.log(errMessage);
-    inputError_div.innerHTML = errMessage;
+    inputError_div.innerHTML = errMessageToShow;
     inputError_div.classList.value = "input-error";
-    setTimeout(() => hideInputCityError(), 6000);
+    setTimeout(() => hideInputCityError(), 7000);
 };
+
+// !        JS Animations
 
 const hideInputCityError = () => {
     inputError_div.classList.value = "hide";
@@ -127,7 +235,8 @@ const setWeather = e => {
             console.log("asdasd");
             setTemperature(data);
         }).catch(e => {
-            console.log(e + " Throwed an error");
+            // console.log(e + " Throwed an error");
+
             showInputCityError(e);
         });
         // .catch(console.log(object));
